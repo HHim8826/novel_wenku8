8aabdd8bca4d7a256ea5e69dff7542db726327e1
HHim8826 <36044108+HHim8826@users.noreply.github.com>
2021-12-23 20:37:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*
+from aiofiles.threadpool import text
 import requests
 import asyncio
 import re
@@ -7,39 +8,45 @@ import aiohttp
 import os
 from tqdm import tqdm
 
-dl_img = True
+dl_img = False
 
 def get_htm(url):
     req = requests.get(url)
     req.encoding = 'gbk'
     text = req.text
-    all_novel_name = re.findall(r'<div id="title">(.*?)</div>',text,re.S)
-    
+    all_novel_name = re.findall(r'<div id="title">(.*?)</div>',text,re.S) 
+
     return text,all_novel_name[0]
 
                 
-def get_novel_title(html,id):
+def get_novel_title(html,novel_id,version):
     novel_title = {}
     html_list = html.split('vcss')
-    novel_compile = re.compile(r'colspan=".*?" vid=".*?">(?P<novel_name>.*?)</td>\r\n',re.S)
-    title_compile = re.compile(r'<td class="ccss"><a href="(?P<url>.*?)">(?P<title_name>.*?)</a></td>',re.S)
+    
+    if version == 2:
+        novel_compile = re.compile(r'colspan=".*?" vid=".*?">(?P<novel_name>.*?)</td>\r\n',re.S)
+    elif version == 1:
+        novel_compile = re.compile(r'colspan=".*?">(?P<novel_name>.*?)</td>\r\n',re.S)
+        
+    title_compile = re.compile(r'<td class="ccss"><a href="(?P<url>.*?)">(?P<title_name>.*?)</a></td>',re.S)  
     
     
     for novel in html_list:
+        
         res_novel = novel_compile.finditer(novel)
         res_title = title_compile.finditer(novel)
         
         for it in res_novel:
             title_list = []
             novel_name = it.group('novel_name')
-            for title in res_title:           
+            
+            for title in res_title:
+                
                 title_anme = title.group('title_name')
                 title_url = title.group('url')
-                
-                title_list.append({title_anme:f'https://www.wenku8.net/novel/2/{id}/{title_url}'})
+                title_list.append({title_anme:f'https://www.wenku8.net/novel/{version}/{novel_id}/{title_url}'})
                 
             novel_title[novel_name] = title_list
-    
     return novel_title
 
 
@@ -123,13 +130,21 @@ async def dl_novel(file,url,name,session,all_novel_name,pbar):
                 pbar.update(1)
         
 
-async def main(id):
-    url = f'https://www.wenku8.net/novel/2/{id}/index.htm'
+async def main(novel_id):
     tasks_long = 0
     tasks = []
-    html_list,all_novel_name = get_htm(url)
-    novel_title = get_novel_title(html_list,id)
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
+    try:
+        version = 2
+        url = f'https://www.wenku8.net/novel/{version}/{novel_id}/index.htm'
+        html,all_novel_name = get_htm(url)
+        novel_title = get_novel_title(html,novel_id,version)
+    except IndexError:
+        version = 1
+        url = f'https://www.wenku8.net/novel/{version}/{novel_id}/index.htm'
+        html,all_novel_name = get_htm(url)
+        novel_title = get_novel_title(html,novel_id,version)
+    
     # print(novel_title)
     
     
@@ -142,10 +157,9 @@ async def main(id):
                 for dict in valeue:                    
                     for key_n,valeue_n in dict.items():
                         tasks.append(asyncio.create_task(dl_novel( key, valeue_n,f'{r}.{key_n}', session, all_novel_name, bar))) 
-                         
                     r += 1
             await asyncio.wait(tasks)
 
 if __name__ =='__main__':
-    id = 2428
-    asyncio.run(main(id))
+    novel_id = 2428 # https://www.wenku8.net/novel/2/{id}/index.htm or https://www.wenku8.net/book/{id}.htm
+    asyncio.run(main(novel_id))
